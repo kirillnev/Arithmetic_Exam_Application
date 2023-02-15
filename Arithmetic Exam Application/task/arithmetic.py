8c39aac6644391d4fee9eb4cb0bcd32b53e28620
Kirill Nevinskii <mail@kirillnev.ru>
2023-02-15 20:08:33 +0600

    Stage_4_4

--- Arithmetic Exam Application/task/arithmetic.py
+++ Arithmetic Exam Application/task/arithmetic.py
@@ -7,9 +7,13 @@ operators = {
     '*': lambda x, y: x * y,
 }
 TASKS_COUNT = 5
+lvl_descr = {
+    '1': "simple operations with numbers 2-9",
+    '2': "integral squares 11-29"
+}
 
 
-def main():
+def start_level1():
     n = 0
     correct_n = 0
     while n < TASKS_COUNT:
@@ -30,7 +34,58 @@ def main():
                 break
             else:
                 print('Incorrect format.')
-    print(f'Your mark is {correct_n}/{TASKS_COUNT}.')
+    return correct_n
+
+
+def start_level2():
+    n = 0
+    correct_n = 0
+    while n < TASKS_COUNT:
+        x = random.randint(11, 29)
+        print(x)
+        while True:
+            answer = input()
+            if answer.isdigit():
+                answer = int(answer)
+                if answer == x**2:
+                    print('Right!')
+                    correct_n += 1
+                else:
+                    print('Wrong!')
+                n += 1
+                break
+            else:
+                print('Incorrect format.')
+    return correct_n
+
+
+def save_result(n, lvl_num):
+    print("What is your name?")
+    name = input()
+    res_str = f"{name}: {n}/5 in level {lvl_num} ({lvl_descr[lvl_num]})."
+    with open('results.txt', 'a') as f:
+        f.write(res_str)
+    print('The results are saved in "results.txt".')
+
+
+def main():
+    while True:
+        print('Which level do you want? Enter a number:')
+        print('1 - simple operations with numbers 2-9')
+        print('2 - integral squares 11-29')
+        lvl_num = input()
+        if lvl_num == '1':
+            correct_n = start_level1()
+            break
+        elif lvl_num == '2':
+            correct_n = start_level2()
+            break
+        else:
+            print('Incorrect format.')
+    print(f'Your mark is {correct_n}/{TASKS_COUNT}. Would you like to save the result? Enter yes or no.')
+    cmd = input()
+    if cmd in ['yes', 'YES', 'y', 'Yes']:
+        save_result(correct_n, lvl_num)
 
 
 if __name__ == '__main__':
