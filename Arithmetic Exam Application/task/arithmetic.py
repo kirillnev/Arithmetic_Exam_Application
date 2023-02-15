9788f853c7c5052ec99163421060bf7d58b8ec0f
Kirill Nevinskii <mail@kirillnev.ru>
2023-02-15 17:39:40 +0600

    Stage_3_4

--- Arithmetic Exam Application/task/arithmetic.py
+++ Arithmetic Exam Application/task/arithmetic.py
@@ -6,14 +6,31 @@ operators = {
     '-': lambda x, y: x - y,
     '*': lambda x, y: x * y,
 }
+TASKS_COUNT = 5
+
 
 def main():
-    x = random.randint(2, 9)
-    y = random.randint(2, 9)
-    operator = random.choice(list(operators.keys()))
-    print(x, operator, y)
-    a = int(input())
-    print('Right!' if a == operators[operator](x, y) else 'Wrong!')
+    n = 0
+    correct_n = 0
+    while n < TASKS_COUNT:
+        x = random.randint(2, 9)
+        y = random.randint(2, 9)
+        operator = random.choice(list(operators.keys()))
+        print(x, operator, y)
+        while True:
+            answer = input()
+            if re.match(r'^-?\d+$', answer):
+                answer = int(answer)
+                if answer == operators[operator](x, y):
+                    print('Right!')
+                    correct_n += 1
+                else:
+                    print('Wrong!')
+                n += 1
+                break
+            else:
+                print('Incorrect format.')
+    print(f'Your mark is {correct_n}/{TASKS_COUNT}.')
 
 
 if __name__ == '__main__':
